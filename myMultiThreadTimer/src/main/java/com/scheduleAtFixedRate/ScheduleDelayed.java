6f814a01734450b390d083b7e386139dec308bd8
zhengchaoqun <zhengchaoqun@ucredit.com>
2017-09-28 11:24:49 +0800

    提交

---
+++ myMultiThreadTimer/src/main/java/com/scheduleAtFixedRate/ScheduleDelayed.java
@@ -0,0 +1,28 @@
+package com.scheduleAtFixedRate;
+
+import java.util.Calendar;
+import java.util.Date;
+import java.util.Timer;
+import java.util.TimerTask;
+
+public class ScheduleDelayed {
+    static class MyTask extends TimerTask {
+        @Override
+        public void run() {
+            try {
+                System.out.println("begin timer=" + System.currentTimeMillis());
+                Thread.sleep(5000);
+                System.out.println("  end timer=" + System.currentTimeMillis());
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        MyTask task = new MyTask();
+        Calendar c = Calendar.getInstance();
+        Date runDate = c.getTime();
+        new Timer().scheduleAtFixedRate(task, runDate, 3000);
+    }
+}
